d91c74c5ea42ce7ecf20b4cc30f41a511356bf7b
Dreat <Fzai3xc2>
2012-12-23 20:22:52 +0100

    JAVANIE

--- java_test/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ java_test/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -3,15 +3,24 @@
  * and open the template in the editor.
  */
 package javaapplication1;
+<<<<<<< HEAD
 import java.io.IOException;
 import java.lang.String;
+=======
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+>>>>>>> 9ba7fa8be8d045fe28ec28c149c18a78cb5f66b8
 
 /**
  *
  * @author mbaza_000
  */
-public class JavaApplication1 {
+public class JavaApplication1
+{
 
+<<<<<<< HEAD
     /**
      * @param args the command line arguments
      */
@@ -20,5 +29,16 @@ public class JavaApplication1 {
         System.out.println("Hello World");
         String chujnia;
         System.in.read();
+=======
+    public static void main(String [] args)
+    {
+        // TODO code application logic here
+        System.out.println("Hello World");
+        try {
+            System.in.read();
+        } catch (IOException ex) {
+            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+>>>>>>> 9ba7fa8be8d045fe28ec28c149c18a78cb5f66b8
     }
 }
