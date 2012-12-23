9ba7fa8be8d045fe28ec28c149c18a78cb5f66b8
mbazaczek <mbazaczek@gmail.com>
2012-12-23 19:21:04 +0100

    fun fun fun

--- java_test/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ java_test/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -3,20 +3,26 @@
  * and open the template in the editor.
  */
 package javaapplication1;
-import java.lang.String;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author mbaza_000
  */
-public class JavaApplication1 {
+public class JavaApplication1
+{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main() {
+    public static void main(String [] args)
+    {
         // TODO code application logic here
         System.out.println("Hello World");
-        String chujnia= System.in.read();
+        try {
+            System.in.read();
+        } catch (IOException ex) {
+            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
