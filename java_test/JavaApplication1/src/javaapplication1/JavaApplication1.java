831db965fff88de8544ff705b90a833638e08aa6
mbazaczek <mbazaczek@gmail.com>
2012-12-23 18:41:47 +0100

    java hehe

---
+++ java_test/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+import java.lang.String;
+
+/**
+ *
+ * @author mbaza_000
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main() {
+        // TODO code application logic here
+        System.out.println("Hello World");
+        String chujnia= System.in.read();
+    }
+}
