b78652cd116589a95a1c2139b7c4d33ddb2ac740
mbazaczek <mbazaczek@gmail.com>
2012-12-23 18:28:19 +0100

    fun fun fun

---
+++ c#test/cszarp_test/cszarp_test/Bobek.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cszarp_test
+{
+    public class Bobek
+    {
+        public string sraj(int i)
+        {
+            return ( "sram" + i.ToString() );
+        }
+    }
+}
--- c#test/cszarp_test/cszarp_test/Program.cs
+++ c#test/cszarp_test/cszarp_test/Program.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace cszarp_test
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int i=10;
+            Bobek bobek1 = new Bobek();
+
+            for (i=0;i<10;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else 
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                Console.WriteLine( bobek1.sraj(i) );
+                Console.ReadLine();
+            }
+      
         }
     }
 }
